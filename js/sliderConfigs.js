c66658117aa1f2f63872866cd159d7709e2bfa24
sajjad <sajjadmoeini97@gmail.com>
2023-09-17 00:34:57 +0330

    fix swiper width

--- js/sliderConfigs.js
+++ js/sliderConfigs.js
@@ -1,15 +1,15 @@
-const swiper = new Swiper(".mySwiper", {
-       slidesPerView: 4,
-       spaceBetween: 30,
-       pagination: {
-              el: ".swiper-pagination",
-              clickable: true,
-       },
-});
 
-window.addEventListener('resize',(e)=>{
-       console.log(e.target.innerWidth);
-       if(e.target.innerWidth == 998){
+function chooseSliderSlideCount(width){
+       if(width > 998){
+              const swiper = new Swiper(".mySwiper", {
+                     slidesPerView: 4,
+                     spaceBetween: 30,
+                     pagination: {
+                            el: ".swiper-pagination",
+                            clickable: true,
+                     },
+              });
+       }else if(width < 998){
               const swiper = new Swiper(".mySwiper", {
                      slidesPerView: 2,
                      spaceBetween: 30,
@@ -18,7 +18,7 @@ window.addEventListener('resize',(e)=>{
                             clickable: true,
                      },
               });
-       }else if(e.target.innerWidth == 576){
+       }else if(width < 576){
               const swiper = new Swiper(".mySwiper", {
                      slidesPerView: 1,
                      spaceBetween: 30,
@@ -28,4 +28,15 @@ window.addEventListener('resize',(e)=>{
                      },
               });
        }
+}
+
+window.addEventListener('load',(event)=>{
+
+console.log(document.body.scrollWidth);
+chooseSliderSlideCount(document.body.scrollWidth)
 })
+window.addEventListener('resize',(event)=>{
+
+    
+       chooseSliderSlideCount(document.body.scrollWidth)
+       })
