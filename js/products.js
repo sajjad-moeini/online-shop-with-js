158c77dfed40dfe9cf942d389f4307b57ed331a9
sajjad <sajjadmoeini97@gmail.com>
2023-09-17 20:43:27 +0330

    update cards

--- js/products.js
+++ js/products.js
@@ -15,7 +15,7 @@ let productsContainer = allInformations.products[0]
 function productsLoader() {
        return productsContainer.map(product => {
               return (`
-              <div class="card col-sm-2 col-md-3 col-lg-4 mx-auto  ">
+              <div class="card col-sm-2 col-md-3 col-lg-4 mx-auto  position-relative">
               <img src=../${product.mainImgSrc}
                      class="card-img-top img-fluid" alt="...">
               <div class="card-body">
@@ -25,13 +25,24 @@ function productsLoader() {
               </div>
 
               <div class="card-body d-flex-centering w-100  p-0 ">
-                     <a href="#" class="card-link">
-                            ${product.price.toLocaleString()} تومان
+                     <a href="#" class="card-link position-relative">
+                     ${product.price.toLocaleString()} تومان
+                         ${product.off ? `
+                         <h6 class="position-absolute off-price">
+                          ${
+                            (product.price - ((product.price * product.off) / 100)).toLocaleString()
+                          } تومان
+                          </h6>
+                          <div class="position-absolute card-price-linethrough"></div>
+                         `:''} 
                      </a>
                      <a href="#" class=" btn btn-primary ">
                             مشاهده
                      </a>
               </div>
+             ${
+              product.off ? (`<div class="position-absolute d-flex-centering bg-danger off-badge">% ${product.off} --</div>`) : ('')
+             } 
        </div>
               `)
        })
@@ -40,9 +51,9 @@ function productsLoader() {
 document.body.insertAdjacentHTML('afterbegin', `
 <header>
 ${firstNavBar(nav, logo)}
-${secondNavbar}
+ ${secondNavbar()}
 </header>
-<main class="row">
+<main class="row w-100">
 <div class="row h-25 px-5">
 <form action="#">
        <div class="row">
