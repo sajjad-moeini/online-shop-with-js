3892fa9038e8f2decf7ea1102bdb84c8ae2f830c
sajjad <sajjadmoeini97@gmail.com>
2023-09-21 20:25:17 +0330

    update product section

---
+++ js/product.js
@@ -0,0 +1,110 @@
+import allInformations from "../pages/informations.js"
+
+let productId = Number(location.search.slice(1))
+
+let product = allInformations.products[0].filter(product=>{return product.id == productId})[0]
+console.log(product);
+
+document.body.insertAdjacentHTML('beforeend',`
+<div class="product-container p-3">
+<div class="row about-product-container px-3">
+       <div class="col-12 col-md-4 product-pics-container p-5">
+              <img src=../${product.mainImgSrc}
+                     class="w-100 img-fluid product-lg-img rounded-3" alt="">
+              <div class="row">
+              ${product.imgSrc.map(img=>{
+                     return(`
+                     <div class="col-4 h-25 sm-img-container">
+                     <img src=../${img}
+                            class="w-100 img-fluid h-100 product-sm-img rounded-3" alt="">
+              </div>
+                     `)
+              })}
+              </div>
+       </div>
+       <div class="col-12 col-md-8 p-md-5 ">
+              <div class="row">
+                     <div class="h1">
+${product.name}
+                     </div>
+                     <div class="h4 mt-4">
+                            درباره محصول :
+                     </div>
+                     <p class="mt-4">
+                   ${product.desc}
+                     </p>
+
+              </div>
+
+       </div>
+
+
+
+</div>
+<div class="h3 text-light mt-4 ps-3">
+       سایز های موجود :
+</div>
+<div class="d-flex flex-wrap sizes-container">
+       <div class="size-box-container">
+              <span>7</span>
+       </div>
+       <div class="size-box-container">
+              <span>32</span>
+       </div>
+       <div class="size-box-container">
+              <span>34</span>
+       </div>
+       <div class="size-box-container">
+              <span>36</span>
+       </div>
+       <div class="size-box-container">
+              <span>38</span>
+       </div>
+       <div class="size-box-container">
+              <span>40</span>
+       </div>
+       <div class="size-box-container">
+              <span>42</span>
+       </div>
+</div>
+
+<div class="w-100 d-flex d-flex-centering">
+       <a class=" btn add-to-cart-btn mx-auto">
+              افزودن به سبد خرید
+       </a>
+</div>
+</div>
+`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let productLgImgElem = document.querySelectorAll('.product-lg-img')
+let productSmImgElem = document.querySelectorAll('.product-sm-img')
+let sizeBox = document.querySelectorAll('.size-box-container')
+
+sizeBox.forEach(btn => {
+       btn.addEventListener('click',(bt)=>{
+         btn.classList.toggle('ordered-size')
+       })
+ })
+ productSmImgElem.forEach(img => {
+        img.addEventListener('mouseenter', e => {
+               let id = e.target.dataset.id
+               productLgImgElem.forEach(img => {
+                      if (img.dataset.id == id) {
+                             img.src = e.target.src
+                      }
+               })
+        })
+ })
--- js/products.js
+++ js/products.js
@@ -12,8 +12,8 @@ let logo = allInformations.mainNavbar.logo
 let productsContainer = allInformations.products[0]
 
 
-function productsLoader() {
-       return productsContainer.map(product => {
+function productsLoader(products) {
+       return products.map(product => {
               return (`
               <div class="card col-sm-2 col-md-3 col-lg-4 mx-auto  position-relative">
               <img src=../${product.mainImgSrc}
@@ -29,112 +29,28 @@ function productsLoader() {
                      ${product.price.toLocaleString()} تومان
                          ${product.off ? `
                          <h6 class="position-absolute off-price">
-                          ${
-                            Math.round( (product.price - ((product.price * product.off) / 100))).toLocaleString()
-                          } تومان
+                          ${Math.round((product.price - ((product.price * product.off) / 100))).toLocaleString()
+                            } تومان
                           </h6>
                           <div class="position-absolute card-price-linethrough"></div>
-                         `:''} 
+                         `: ''} 
                      </a>
-                     <a href="#" class=" btn btn-primary " data-bs-toggle="modal" data-bs-target="#productModal${product.id}"> 
+                     <a href="product.html?${product.id}" class=" btn btn-primary " > 
                             مشاهده
                      </a>
               </div>
-             ${
-              product.off ? (`<div class="position-absolute d-flex-centering bg-danger off-badge">% ${product.off} --</div>`) : ('')
-             } 
+             ${product.off ? (`<div class="position-absolute d-flex-centering bg-danger off-badge">% ${product.off} --</div>`) : ('')
+                     } 
        </div>
-       <div class="modal fade" id="productModal${product.id}" tabindex="-1">
-       <div class="modal-dialog modal-lg modal-dialog-centered mx-auto">
-         <div class="modal-content">
-         <div class="modal-header">
-         <button type="button" class="btn-close" data-bs-dismiss="modal" ></button>
-       </div>
-         <div class="product-container p-3">
-         <div class="row about-product-container px-3">
-                <div class="col-12 col-md-4 product-pics-container p-5">
-                       <img src=../${product.mainImgSrc}
-                              class="w-100 img-fluid product-lg-img rounded-3" alt="">
-                       <div class="row">
-
-                       ${product.imgSrc.map(img=>{
-                            return(
-                                   `
-                                   <div class="col-4 h-25 sm-img-container">
-                                   <img src=../${img}
-                                          class="w-100 img-fluid h-100 product-sm-img rounded-3" alt="">
-                            </div>
-                                   `
-                            )
-                       }).join('')}
-                       </div>
-                </div>
-                <div class="col-12 col-md-8 p-md-5 ">
-                       <div class="row">
-                              <div class="h1">
-${product.name}
-                              </div>
-                              <div class="h4 mt-4">
-                                     درباره محصول :
-                              </div>
-                              <p class="mt-4">
-                           ${product.desc}
-                              </p>
-
-                       </div>
-
-                </div>
-
-
-
-         </div>
-         <div class="h3 text-light mt-4 ps-3">
-                سایز های موجود :
-         </div>
-         <div class="d-flex flex-wrap sizes-container">
-                <div class="size-box-container">
-                       <span>30</span>
-                </div>
-                <div class="size-box-container">
-                       <span>32</span>
-                </div>
-                <div class="size-box-container">
-                       <span>34</span>
-                </div>
-                <div class="size-box-container">
-                       <span>36</span>
-                </div>
-                <div class="size-box-container">
-                       <span>38</span>
-                </div>
-                <div class="size-box-container">
-                       <span>40</span>
-                </div>
-                <div class="size-box-container">
-                       <span>42</span>
-                </div>
-                <div class="size-box-container">
-                       <span>44</span>
-                </div>
-         </div>
-
-         <div class="w-100 d-flex d-flex-centering">
-                <a class=" btn add-to-cart-btn mx-auto" data-bs-dismiss="modal">
-                       افزودن به سبد خرید
-                </a>
-         </div>
-  </div>
-           
-         </div>
-       </div>
-     </div>
+       
        `)
        })
+       
 }
 
 document.body.insertAdjacentHTML('afterbegin', `
 <header>
-${firstNavBar(nav, logo,"../index.html")}
+${firstNavBar(nav, logo, "../index.html")}
  ${secondNavbar()}
 </header>
 <main class="row w-100">
@@ -166,7 +82,7 @@ ${firstNavBar(nav, logo,"../index.html")}
 </form>
 </div>
 <div class="cards-container row ">
-${productsLoader()
+${productsLoader(productsContainer)
        }
 </div>
 </main>
@@ -181,11 +97,9 @@ ${companys(allInformations.companys.sidePageImgSrc)}
 let allCtegorySelectBoxElem = document.querySelector('.allCategory')
 let allSortingSelectBoxElem = document.querySelector('.allSorting')
 
-
-
 allCtegorySelectBoxElem.addEventListener('change', (e) => {
        categoryChangeHandler(e.target.value, 'category')
-       allSortingSelectBoxElem.value =-1
+       allSortingSelectBoxElem.value = -1
 })
 
 allSortingSelectBoxElem.addEventListener('change', (e) => {
@@ -196,10 +110,10 @@ allSortingSelectBoxElem.addEventListener('change', (e) => {
 
 function categoryChangeHandler(category, action) {
        if (action == 'category') {
-              productsContainer = allInformations.products[0].filter(product => product.category == category)
+              let filtredProducts = allInformations.products[0].filter(product => product.category == category)
               let productsContainerElem = document.querySelector('.cards-container')
               productsContainerElem.innerHTML = ''
-              productsContainerElem.innerHTML = `${productsLoader()}`
+              productsContainerElem.innerHTML = `${productsLoader(filtredProducts)}`
        }
        if (action == 'sorting') {
               if (category == 'mostSaled') {
@@ -208,25 +122,24 @@ function categoryChangeHandler(category, action) {
               }
               if (category == 'lowerPrice') {
                      let prices = productsContainer.map(product => { return product.price }).sort((a, b) => a - b)
-                     productsContainer = prices.map(price => {
+                     let filtredProducts = prices.map(price => {
                             return productsContainer.find(item => item.price == price)
                      })
                      let productsContainerElem = document.querySelector('.cards-container')
                      productsContainerElem.innerHTML = ''
-                     productsContainerElem.innerHTML = `${productsLoader()}`
+                     productsContainerElem.innerHTML = `${productsLoader(filtredProducts)}`
               }
               if (category == 'highPrice') {
                      let prices = productsContainer.map(product => { return product.price }).sort((a, b) => a - b).reverse()
-                     productsContainer = prices.map(price => {
+                     let filtredProducts = prices.map(price => {
                             return productsContainer.find(item => item.price == price)
                      })
                      let productsContainerElem = document.querySelector('.cards-container')
                      productsContainerElem.innerHTML = ''
-                     productsContainerElem.innerHTML = `${productsLoader()}`
-               }
+                     productsContainerElem.innerHTML = `${productsLoader(filtredProducts)}`
+              }
        }
 }
 
-// window.onload=()=>{productsLoader()}
 
 
