b22fd3a22f6959b514f4486ba0fd5f733600136d
sajjad <sajjadmoeini97@gmail.com>
2023-09-22 17:45:26 +0330

    update searchbar actions

--- js/app.js
+++ js/app.js
@@ -69,6 +69,9 @@ searchbar.addEventListener('keyup', (e) => {
            
 `
 })
-searchbar.addEventListener('blur',()=>{
+document.body.addEventListener('click',()=>{
        searchResultsContainer.style.display='none'
+})
+searchbar.addEventListener('blur',()=>{
+     
 })
